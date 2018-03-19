6411d2174178df186c504708e448e9a16dc9bcf3
Zebux <zebux@zebux.org>
2018-03-19 19:22:16 +0100

    Add optparse to manage arg

--- monitor-ct.py
+++ monitor-ct.py
@@ -14,25 +14,42 @@ import sys
 import json
 from datetime import datetime, timedelta
 
-
-
-version = "v0.1"
+version = "v0.2"
 nbr_days= 7
- 
-if len (sys.argv) < 2:     
-  print "usage: " + sys.argv[0] + " [domains to check]"
-  sys.exit(1)
- 
-if "-t" in sys.argv[1]:
-  nbr_days=int(sys.argv[2])
+
+from optparse import OptionParser
+usage = "usage: %prog [options] filenames / domain ..."
+parser = OptionParser(usage)
+#parser = OptionParser()
+parser.add_option("-d", dest="domain", help="domain name to check")
+parser.add_option("-v", "--version", action="store_true", dest="version", help="display the version")
+parser.add_option("-f", dest="filenamearg", help="mass domain monitor option. File with one line per domain.")
+parser.add_option("-t", type='int', dest="time", default=7, help="check certificate issue since <days number> days - default 7 days")
+
+(options, args) = parser.parse_args()
+'''
+print 'domain     :', options.domain
+print 'version    :', options.version
+print 'file       :', options.filenamearg
+print 'time       :', options.time
+print 'Args :', args
+'''
+
+if options.version :
+  print ""
+  print "This script monitor certificat transparency from a domain"
+  print ""
+  print   sys.argv[0] +" - version : " + version
+  print ""
+  print ""
+  sys.exit(0)
+
+if options.time :
+  nbr_days=int(options.time)
 
 today=datetime.now().isoformat()
 pastdate=datetime.today() - timedelta(days=nbr_days)
 past_date=pastdate.isoformat()
-#print today
-#print past_date
-#print len(sys.argv)
-
 
 def check_ct(dom):
 	req = requests.get("https://crt.sh/?q=%."+ dom +"&output=json")
@@ -46,55 +63,24 @@ def check_ct(dom):
 	  cert=value['name_value']
 	  date_emis=value['not_before']
 	  issuer=value['issuer_name']
-	  #print value['name_value'] + " " + value['not_before']
+	  #print issuer
 	  if date_emis > past_date :
-	    print cert + "\t" + date_emis + "\t" + issuer
+	    try : 
+	      bcn,acn=issuer.split("CN=")
+	      #print value['name_value'] + " " + value['not_before']
+	    except ValueError:
+	      print "Oops!  No CN in issuer !"
+	      acn=issuer
+	    print cert + "\t" + date_emis + "\t" + acn
 
-if "-h" in sys.argv or "--help" in sys.argv:
-  print "usage : "+sys.argv[0]+ " [-d] domain"
-  print ""
-  print "This script monitor certificat transparency from a domain"
-  print ""
-  print "Optional arguments:"
-  print "   -h, --help    show this help message and exit"
-  print "   -v, --version show version."
-  print "   -f <filename>, Mass domain monitor option. File with one line per domain."
-  print "   -t <days number>, Check certificat issue since <days number> days - default 7 days "
-  sys.exit(0)
-
-if "-v" in sys.argv or "--version" in sys.argv:
-  print ""
-  print ""
-  print "This script monitor certificat transparency from a domain"
-  print ""
-  print " sys.argv[0] - version : " + version
-  print ""
-  print ""
-  sys.exit(0)
-  
-if "-d" in sys.argv[1] :
-  domain = sys.argv[2]
+if options.domain :
+  domain = options.domain
   check_ct(domain)
   sys.exit(0)
-
-if len (sys.argv) == 5:
-	if "-d" in sys.argv[3] :
-	  domain = sys.argv[4]
-	  check_ct(domain)
-	  sys.exit(0)
-	if "-f" in sys.argv[3]:
-		filename=sys.argv[4]
-		with open(filename) as fp:
-			for domain in fp:
-				check_ct(domain.rstrip())
-	sys.exit(0)
-
-if "-f" in sys.argv[1]:
-  filename=sys.argv[2]
+ 
+if options.filenamearg :
+  filename=options.filenamearg
   with open(filename) as fp:
     for domain in fp:
       check_ct(domain.rstrip())
-  sys.exit(0)
-
-
-
+  sys.exit(0)  
